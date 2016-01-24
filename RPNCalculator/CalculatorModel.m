9cdc1c71b7819113ca3f94fa13cf0853b95bb9f3
Rosie <Rosie@roses-macbook-pro.local>
2016-01-24 10:13:21 -0500

    FOUND ISSUE: everything is operating correctly except the array is not actually being filled, therefore it returns null when operands are pulled resulting in a value of zero at all times

--- RPNCalculator/CalculatorModel.m
+++ RPNCalculator/CalculatorModel.m
@@ -27,18 +27,21 @@
 // Push operand to the stack
 -(void) pushOperand: (double)operand{
     [self.operandStack addObject: [NSNumber numberWithDouble:operand]]; // Add NSNumber object to the array
+    NSLog(@"Operands on stack: %f", operand);
+    NSLog(@"Array: %@", _operandStack); // FOUND ISSUE: everything is operating correctly except the array is not actually being filled, therefore it returns null when operands are pulled resulting in a value of zero at all times 
 }
 
 -(double) popOperand {
     NSNumber *operandObject = [self.operandStack lastObject]; // Looks for last object
     if (operandObject) [self.operandStack removeLastObject];  // Removes last object from the array
+    NSLog(@"Operand Pulled From Stack: %@", operandObject);
     return [operandObject doubleValue]; // Returns the double value of the last object
 }
 
 // Perform a given operation
 -(double) performOperation: (NSString *)operation{
     // calculate results
-    double result = 0;
+    double result;
     if ([operation isEqualToString:@"+"]){
         result = [self popOperand] + [self popOperand];
     } else if ([@"*" isEqualToString:operation]){
--- RPNCalculator/ViewController.m
+++ RPNCalculator/ViewController.m
@@ -12,7 +12,7 @@
 @interface ViewController ()
 
 @property (nonatomic) BOOL userIsInTheMiddleOfEnteringANumber;
-@property (nonatomic) CalulatorModel *brain;
+@property (nonatomic, strong) CalulatorModel *brain;
 
 @end
 
@@ -32,26 +32,32 @@
     NSString *digit = [sender currentTitle]; // current title copied into local variable digit
     if (self.userIsInTheMiddleOfEnteringANumber){
         self.display.text = [self.display.text stringByAppendingString:digit]; // my displays text is equal to the current display text with the new appended digit (utalizes a setter and getter)
+        NSLog(@"Digit Pressed: %@", digit); // digit is being recognized
     } else {
         self.display.text = digit;
         self.userIsInTheMiddleOfEnteringANumber = YES;
+        NSLog(@"Digit Pressed: %@", digit); // digit is being recognized
     }
 }
 
 // OPERATION IS BEING PRESSD ACTION
 - (IBAction)operationPressed:(UIButton *)sender {
     
-    if (self.userIsInTheMiddleOfEnteringANumber) [self enterPressed]; // press enter
+    if (self.userIsInTheMiddleOfEnteringANumber){
+        [self enterPressed]; // press enter
+    } // enter is pressed when user is in the middle of typing a number
     
     double result = [self.brain performOperation:sender.currentTitle]; // result as a double
     NSString *resultString = [NSString stringWithFormat:@"%g", result]; // result as a string
     self.display.text = resultString;
+    NSLog(@"Resulting String: %@", resultString);
 }
 
 // ENTER BUTTON PRESSED
 - (IBAction)enterPressed {
     [self.brain pushOperand:[self.display.text doubleValue]]; // send double value to push operand
     self.userIsInTheMiddleOfEnteringANumber = NO;
+    NSLog(@"Enter has been pressed.");
 }
 
 @end
