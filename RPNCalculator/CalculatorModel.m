36480e4cacc11f2ff1c50901223c80b923eead28
Rosie <Rosie@roses-macbook-pro.local>
2016-01-24 00:01:26 -0500

    Added the first operation, plus, into the calculator model -> need to add the rest of the operations, need to add the enter key

--- RPNCalculator/CalculatorModel.m
+++ RPNCalculator/CalculatorModel.m
@@ -9,15 +9,41 @@
 #import <Foundation/Foundation.h>
 #import "CalulatorModel.h"
 
+// PRIVATE INTERFACE
+@interface CalulatorModel()
+@property (nonatomic, strong) NSMutableArray *operandStack; // Operand stack
+@end
+
 @implementation CalulatorModel
 
--(void) pushOperand: (double)operand{
-    
+@synthesize operandStack = _operandStack;
+
+// Getter for operand stack
+-(NSMutableArray *) operandStack{
+    if ((_operandStack = nil)) _operandStack = [[NSMutableArray alloc] init]; // if nill then allocate it
+    return _operandStack;
 }
 
+// Push operand to the stack
+-(void) pushOperand: (double)operand{
+    [self.operandStack addObject: [NSNumber numberWithDouble:operand]]; // Add NSNumber object to the array
+}
+
+-(double) popOperand {
+    NSNumber *operandObject = [self.operandStack lastObject]; // Looks for last object
+    if (operandObject) [self.operandStack removeLastObject];  // Removes last object from the array
+    return [operandObject doubleValue]; // Returns the double value of the last object
+}
+
+// Perform a given operation
 -(double) performOperation: (NSString *)operation{
     double result = 0;
-    // calculate results 
+    // calculate results
+    
+    if ([operation isEqualToString:@"➕"]){
+        result = [self popOperand] + [self popOperand];
+    }
+    
     return result;
 }
 
